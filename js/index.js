ce1288b00cb99fb68dce344eb283ebd75045f265
Mohit Goyal <mohitgoyal707@gmail.com>
2020-05-20 21:44:05 +0530

    wip

--- js/index.js
+++ js/index.js
@@ -1,1 +1,1 @@
-window.location.replace("market://details?id=com.application.zomato&referrer=zomato%3A%2F%2Fgold_home");
+window.location.replace("market://details?id=com.application.zomato&referrer=gold_home");
