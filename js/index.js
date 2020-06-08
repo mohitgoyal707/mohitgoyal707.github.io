6fb6830eee24db1c2507a7e4751ed34b857c5c95
Mohit Goyal <mohitgoyal707@gmail.com>
2020-06-08 17:03:43 +0530

    update

--- js/index.js
+++ js/index.js
@@ -1,2 +1,2 @@
-let url = "https://bnc.lt/contactless?deeplink_path=https%3A%2F%2Fzoma.to%2Fqr%2FABCDEFGH";
+let url = "https://bnc.lt/contactlessdining?deeplink_path=https%3A%2F%2Fzoma.to%2Fqr%2FABCDEFGH";
 window.location.replace(url);
