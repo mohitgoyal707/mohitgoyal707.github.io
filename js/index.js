4a4684684ae2dd647ebcf60000962d69bb597ffb
Mohit Goyal <mohitgoyal707@gmail.com>
2020-06-08 13:47:27 +0530

    update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,3 @@
 //let url = "market://details?id=com.application.zomato&referrer=zomato%3A%2F%2Fgold_home";
-let url = "www.google.com";
+let url = "https://www.google.com";
 window.location.replace(url);
