0a197e6a374c969148121a2b649b6e466f0b578a
Mohit Goyal <mohitgoyal707@gmail.com>
2022-01-29 01:33:36 +0530

    update

--- js/index.js
+++ js/index.js
@@ -1,2 +1,2 @@
-let url = "https://bnc.lt/contactlessdining?deeplink_url=https%3A%2F%2Fzoma.to%2Fqr%2FABCDEFGH";
+let url = "https://bnc.lt/WJml/Fdl5uXIswgb?deeplink_url=zomato%3A%2F%2Fred_cart%3Fis_activation_code_applied%3D0%26requires_login%3D1%26use_payment_sdk%3D%26voucher_code%3DANIKET9W%26extra_params%3D%257B%2522campaign_id%2522%253A20%252C%2522should_show_choice_snippet%2522%253Atrue%257D";
 window.location.replace(url);
